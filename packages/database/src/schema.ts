c304adf786e69cd477ec56950fe72e7bdac2d7c0
Kian Bazarjani <kian@bazza.dev>
2024-10-22 15:23:44 -0400

    Add base schema for auth

--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -1,27 +1,20 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, integer, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { InferSelectModel } from 'drizzle-orm/table'
 
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
-});
+const usersTable = pgTable('users', {
+  id: uuid('id').primaryKey(),
+})
 
-export const postsTable = pgTable('posts_table', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
+const sessionsTable = pgTable('sessions', {
+  id: uuid('id').primaryKey(),
   userId: integer('user_id')
     .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .$onUpdate(() => new Date()),
-});
+    .references(() => usersTable.id),
+  expiresAt: timestamp('expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+})
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
-
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export type User = InferSelectModel<typeof usersTable>
+export type Session = InferSelectModel<typeof sessionsTable>
