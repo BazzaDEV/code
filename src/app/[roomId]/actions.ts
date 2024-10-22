cbf718158f076e962b29c3ff71c5f09099b43e2b
Kian Bazza <kian@bazza.dev>
2024-10-22 12:07:01 -0400

    chore: fresh start for repo (#28)
    Fresh start

--- src/app/[roomId]/actions.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import { uniqueNamesGenerator, animals } from 'unique-names-generator'
-
-export async function randomUsername() {
-  const random = uniqueNamesGenerator({ dictionaries: [animals] })
-  return random
-}
