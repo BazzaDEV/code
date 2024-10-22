b0cf4d5343351dd4b13943184c120488b54558d1
Kian Bazarjani <kian@bazza.dev>
2024-10-22 12:04:33 -0400

    Fresh start

--- src/lib/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, JetBrains_Mono } from 'next/font/google'
-
-export const inter = Inter({ subsets: ['latin'] })
-
-export const jetbrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  variable: '--font-jetbrains-mono',
-})
