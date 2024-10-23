165ba1db71ac48a8b1b55f5ab755da91da233dc2
Kian Bazarjani <kian@bazza.dev>
2024-10-23 16:43:19 -0400

    Progress

--- apps/api/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@avelin/eslint-config/index.js'],
-}
--- apps/api/src/client.ts
+++ apps/api/src/client.ts
@@ -1,4 +1,8 @@
 import { AppType } from './app'
 import { hc } from 'hono/client'
 
-export const client = hc<AppType>('http://localhost:4000/')
+if (!process.env.API_URL) {
+  throw new Error('API_URL is not set')
+}
+
+export const client = hc<AppType>(process.env.API_URL)
