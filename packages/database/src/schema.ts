091bff985e41ad710f2c547dc60ee031608e0f88
Kian Bazarjani <kian@bazza.dev>
2024-10-23 17:57:09 -0400

    Infer db column names

--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -1,16 +1,16 @@
-import { pgTable, integer, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { pgTable, timestamp, uuid } from 'drizzle-orm/pg-core'
 import { InferSelectModel } from 'drizzle-orm/table'
 
 const users = pgTable('users', {
-  id: uuid('id').primaryKey(),
+  id: uuid().primaryKey(),
 })
 
 const sessions = pgTable('sessions', {
-  id: uuid('id').primaryKey(),
-  userId: uuid('user_id')
+  id: uuid().primaryKey(),
+  userId: uuid()
     .notNull()
     .references(() => users.id),
-  expiresAt: timestamp('expires_at', {
+  expiresAt: timestamp({
     withTimezone: true,
     mode: 'date',
   }).notNull(),
