daa6975e227cdd7fad9b8dec690077a97e97f365
Kian Bazarjani <kian@bazza.dev>
2024-10-23 20:50:02 -0400

    Switch to pnpm catalog

--- packages/auth/src/session.ts
+++ packages/auth/src/session.ts
@@ -14,35 +14,44 @@ export async function createSession(userId: string): Promise<Session> {
     .insert(schema.sessions)
     .values(session)
     .returning()
+
+  if (!createdSession) {
+    throw new Error('Failed to create session')
+  }
+
   return createdSession
 }
 
-export async function validateSession(
-  sessionId: string,
-): Promise<SessionValidationResult> {
+export async function validateSession(sessionId: string) {
   const result = await db
     .select({ user: schema.users, session: schema.sessions })
     .from(schema.sessions)
     .innerJoin(schema.users, eq(schema.sessions.userId, schema.users.id))
     .where(eq(schema.sessions.id, sessionId))
 
-  const { user, session } = result[0]
-
-  if (Date.now() >= session.expiresAt.getTime()) {
-    await db.delete(schema.sessions).where(eq(schema.sessions.id, sessionId))
-    return null
-  }
-
-  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
-    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
-    await db
-      .update(schema.sessions)
-      .set({
-        expiresAt: session.expiresAt,
-      })
-      .where(eq(schema.sessions.id, sessionId))
-  }
-  return { session, user }
+  // const result = await db
+  //   .select({ user: schema.users, session: schema.sessions })
+  //   .from(schema.sessions)
+  //   .innerJoin(schema.users, eq(schema.sessions.userId, schema.users.id))
+  //   .where(eq(schema.sessions.id, sessionId))
+  //
+  // const { user, session } = result[0]
+  //
+  // if (Date.now() >= session.expiresAt.getTime()) {
+  //   await db.delete(schema.sessions).where(eq(schema.sessions.id, sessionId))
+  //   return null
+  // }
+  //
+  // if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+  //   session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
+  //   await db
+  //     .update(schema.sessions)
+  //     .set({
+  //       expiresAt: session.expiresAt,
+  //     })
+  //     .where(eq(schema.sessions.id, sessionId))
+  // }
+  // return { session, user }
 }
 
 export async function invalidateSession(sessionId: string): Promise<void> {
